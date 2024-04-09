543158cf4510161bc047bba2913dac5223f7b650
Madhusudhan K <madhumk0709m@gmail.com>
2024-04-09 19:41:54 +0530

    simple form validation

--- app.py
+++ app.py
@@ -1,9 +1,35 @@
-from flask import Flask
+from flask import Flask,redirect,url_for,render_template,request
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
+def home():
     return 'Hello, World!'
+
+@app.route('/submit',methods=['POST','GET'])
+def submit():
+    if request.method == 'POST':
+        name = str(request.form['name'])
+        age = str(request.form['age'])
+        res = f'my name is {name} and age is {age}'
+        return render_template('index.html',res=res)
+        
+
+# @app.route('/success/<int:score>')
+# def success(score):
+#     return "the person has passed and the marks is "+str(score)
+
+# @app.route('/success/<int:score>')
+# def fail(score):
+#     return "the person has failed and the marks is "+str(score)
+
+@app.route('/success/<int:marks>')
+def result(marks):
+    if marks >=50:
+        result = "passed"
+    else:
+        result = "fail"
+    # return f' the result is {result} score is {score}'
+    return render_template('index.html', marks=marks, result=result)
 
 if __name__ == '__main__':
     app.run(debug=True)
