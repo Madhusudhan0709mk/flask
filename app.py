744ecdfc0999d68fc8d1bbbd154f871dcd0de58c
Madhusudhan K <madhumk0709m@gmail.com>
2024-04-09 20:26:43 +0530

    simple form validation

--- app.py
+++ app.py
@@ -1,17 +1,22 @@
 from flask import Flask,redirect,url_for,render_template,request
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/',methods=['POST','GET'])
 def home():
-    return 'Hello, World!'
-
-@app.route('/submit',methods=['POST','GET'])
-def submit():
+    res = ''
     if request.method == 'POST':
         name = str(request.form['name'])
         age = str(request.form['age'])
-        res = f'my name is {name} and age is {age}'
-        return render_template('index.html',res=res)
+        res =  f' the result is {name} score is {age}'
+    return render_template('index.html', res=res)
+
+# @app.route('/submit',methods=['POST','GET'])
+# def submit():
+#     if request.method == 'POST':
+#         name = str(request.form['name'])
+#         age = str(request.form['age'])
+#         res = f'my name is {name} and age is {age}'
+#         return render_template('index.html',res=res)
         
 
 # @app.route('/success/<int:score>')
@@ -22,14 +27,14 @@ def submit():
 # def fail(score):
 #     return "the person has failed and the marks is "+str(score)
 
-@app.route('/success/<int:marks>')
-def result(marks):
-    if marks >=50:
-        result = "passed"
-    else:
-        result = "fail"
-    # return f' the result is {result} score is {score}'
-    return render_template('index.html', marks=marks, result=result)
+# @app.route('/success/<int:marks>')
+# def result(marks):
+#     if marks >=50:
+#         result = "passed"
+#     else:
+#         result = "fail"
+#     # return f' the result is {result} score is {score}'
+#     return render_template('index.html', marks=marks, result=result)
 
 if __name__ == '__main__':
     app.run(debug=True)
